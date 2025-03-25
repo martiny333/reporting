d528bd35e982472dab114a21574ca19e78a5c20b
DR-Gong <139174952+martiny333@users.noreply.github.com>
2025-03-25 09:15:08 -0400

    Update marctab.sql

--- queries/marctab.sql
+++ queries/marctab.sql
@@ -1,1 +1,19 @@
+--ldp:function get_users
 
+CREATE FUNCTION get_users(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    id text,
+    barcode text,
+    created_date timestamp with time zone)
+AS $$
+SELECT id::text, barcode, created_date
+
+    FROM user_users
+    WHERE user_users.created_date >= start_date AND user_users.created_date <= end_date
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
