ca439d9ee7616ae77af7c71ef88c96205d1cf393
DR-Gong <139174952+martiny333@users.noreply.github.com>
2025-04-06 12:53:01 -0400

    Update items.sql

--- queries/items.sql
+++ queries/items.sql
@@ -1,1 +1,19 @@
+--ldp:function items
 
+CREATE FUNCTION items(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    item_hrid text,
+    barcode text,
+    created_date timestamp with time zone,
+    updated_date timestamp with time zone )
+AS $$
+SELECT item_hrid, barcode, created_date, updated_date
+    FROM folio_reporting.item_ext
+    WHERE WHERE updated_date > start_date and updated_date <= end_date
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
