f1c2bac0cb521f5a770e380cec9741741d1e6236
DR-Gong <139174952+martiny333@users.noreply.github.com>
2025-04-17 08:31:42 -0400

    Create expenditure.sql

---
+++ queries/expenditure.sql
@@ -0,0 +1,19 @@
+--ldp:function items
+
+CREATE FUNCTION items(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01'
+)
+RETURNS TABLE(
+    id text,
+    created_date timestamp with time zone,
+    barcode text)
+AS $$
+SELECT id::text, created_date, barcode
+
+    FROM item_ext ie
+    WHERE ie.created_date >= start_date AND ie.created_date <= end_date
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
