1dd19c4db9035dc55c4ce856f0630850028d4850
DR-Gong <139174952+martiny333@users.noreply.github.com>
2025-08-15 08:29:06 -0400

    Update test2.sql

--- queries/test2.sql
+++ queries/test2.sql
@@ -1,18 +1,18 @@
---ldp:function get_users2
+--metadb:function get_users2
 
 CREATE FUNCTION get_users2(
     start_date date DEFAULT '2000-01-01',
     end_date date DEFAULT '2050-01-01'
 )
 RETURNS TABLE(
-    id text,
+    user_id text,
     barcode text,
     created_date timestamp with time zone)
 AS $$
-SELECT id::text, barcode, created_date
+SELECT ug.user_id::text, ug.barcode, ug.created_date
 
-    FROM user_users
-    WHERE user_users.created_date >= start_date AND user_users.created_date <= end_date
+    FROM folio_derived.user_groups ug
+    WHERE ug.created_date >= start_date AND ug.created_date <= end_date
 $$
 LANGUAGE SQL
 STABLE
